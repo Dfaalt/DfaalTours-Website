cd8ac91f300e0275d0845840896d26b83fac901b
Ilham Maulana <ilhammln1510@gmail.com>
2024-01-06 19:50:57 +0700

    update

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-const nextBtn = document.querySelector('.next-btn');
-const prevBtn = document.querySelector('.prev-btn');
-const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
 const numberofSlides = slides.length;
-let slideNumber = sessionStorage.getItem('slideNumber') || 0; // Ambil slideNumber dari sessionStorage jika tersedia
+let slideNumber = sessionStorage.getItem("slideNumber") || 0; // Ambil slideNumber dari sessionStorage jika tersedia
 
 // Fungsi untuk menandai slide aktif
 const showSlide = () => {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    });
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
 
-    slides[slideNumber].classList.add('active');
-}
+  slides[slideNumber].classList.add("active");
+};
 
 // Fungsi untuk menyimpan slideNumber ke sessionStorage
 const saveSlideNumberToSessionStorage = () => {
-    sessionStorage.setItem('slideNumber', slideNumber);
-}
+  sessionStorage.setItem("slideNumber", slideNumber);
+};
 
 //slider next button
 nextBtn.onclick = () => {
-    slideNumber++;
+  slideNumber++;
 
-    if (slideNumber > (numberofSlides - 1)) {
-        slideNumber = 0;
-    }
+  if (slideNumber > numberofSlides - 1) {
+    slideNumber = 0;
+  }
 
-    saveSlideNumberToSessionStorage();
-    showSlide();
-}
+  saveSlideNumberToSessionStorage();
+  showSlide();
+};
 
 //slider prev button
 prevBtn.onclick = () => {
-    slideNumber--;
+  slideNumber--;
 
-    if (slideNumber < 0) {
-        slideNumber = numberofSlides - 1;
-    }
+  if (slideNumber < 0) {
+    slideNumber = numberofSlides - 1;
+  }
 
-    saveSlideNumberToSessionStorage();
-    showSlide();
-}
+  saveSlideNumberToSessionStorage();
+  showSlide();
+};
 
 // Tampilkan slide awal ketika halaman dimuat
 showSlide();
-
-
